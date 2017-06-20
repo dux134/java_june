6774e9e64a1ee991c6c34b14044e6c9d2fdf4d95
Prashant Chaudhary <101.prashant@gmail.com>
2017-06-20 16:24:49 +0530

    Drawing demo

--- DrawingDemo/src/codekamp/Demo.java
+++ DrawingDemo/src/codekamp/Demo.java
@@ -2,11 +2,17 @@ package codekamp;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * Created by cerebro on 19/06/17.
  */
-public class Demo {
+public class Demo implements KeyListener {
+
+    public static int xVel = 3;
+    public static int yVel = 3;
+
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
@@ -27,27 +33,89 @@ public class Demo {
             e.printStackTrace();
         }
 
+        p.setFocusable(true);
+        p.requestFocus();
 
-        Graphics g1 = p.getGraphics();
+        Demo d1 = new Demo();
+        p.addKeyListener(d1);
 
-        g1.setColor(Color.cyan);
-        g1.fillRect(0, 0, 500, 400);
+        int x = 0;
+        int y = 0;
 
-        g1.setColor(Color.yellow);
+        while (true) {
 
-        //top left pacman
-        g1.fillArc(0, 0, 100, 100, 0, 270);
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        //top right
-        g1.fillArc(400, 0, 100, 100, -90, 270);
-
-        //bottom left
-        g1.fillArc(0, 300, 100, 100, 90, 270);
-
-        //bottom right
-        g1.fillArc(400, 300, 100, 100, 180, 270);
+            x += Demo.xVel;
+            y += Demo.yVel;
 
 
-        g1.dispose();
+            if(y >= 300) {
+                y = 300;
+                Demo.yVel = -3;
+            }
+
+            if(x >= 400) {
+                x = 400;
+                Demo.xVel = -3;
+            }
+
+            if(y <= 0) {
+                y = 0;
+                Demo.yVel = 3;
+            }
+
+            if(x <= 0) {
+                x = 0;
+                Demo.xVel = 3;
+            }
+
+            Graphics g1 = p.getGraphics();
+            g1.clearRect(0, 0, 500, 400);
+            g1.setColor(Color.cyan);
+            g1.fillRect(0, 0, 500, 400);
+            g1.setColor(Color.yellow);
+            g1.fillOval(x, y, 100, 100);
+            g1.dispose();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // do nothing
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_RIGHT:
+                Demo.xVel = 3;
+                Demo.yVel = 0;
+                break;
+            case KeyEvent.VK_LEFT:
+                Demo.xVel = -3;
+                Demo.yVel = 0;
+                break;
+            case KeyEvent.VK_UP:
+                Demo.xVel = 0;
+                Demo.yVel = -3;
+                break;
+            case KeyEvent.VK_DOWN:
+                Demo.xVel = 0;
+                Demo.yVel = 3;
+                break;
+            default:
+                System.out.println("some other key pressed");
+
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // do nothing
     }
 }
